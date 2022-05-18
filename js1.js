98eba78c423b108a5e97c7af5d6332dd1052c07a
wonhyejong <zukommen@gmail.com>
2022-05-18 17:22:30 +0900

    last

---
+++ js1.js
@@ -0,0 +1,36 @@
+// 한줄 주석 
+/*
+주석 여러줄
+로 작성 
+*/
+
+let user=true;
+let userTelnumber;
+let userAddress;
+
+let num=50;
+let str='자바스크립트';
+let value1=123+'2022'; //타입 문자 
+let value2=2022/'2023'; //NaN 타입 숫자 
+console.log(typeof value1);
+console.log(typeof value2);
+
+let str1 = 2030;
+let str2 = '안녕'+str1+'\n년';
+/* 템플릿 리터럴 벡틱 ` */
+let str3 = `안녕하세요 올     해는
+ ${str1} 년입니다.` 
+console.log(str2)
+
+/* boolean 블리언  */
+
+let isOn=true;
+let isOff=false;
+console.log(isOn)
+console.log(typeof isOff)
+
+let js1;
+console.log(typeof js1)
+
+
+
---
+++ js2.js
@@ -0,0 +1,14 @@
+/* 자바스크립트에서 거짓으로 취급하는 값 */
+/* 0,-0,null,undefinded,NaN,'' */
+
+/* 오브젝트 객체 */
+let user = {
+    name:'kim',
+    age:50,
+    address:'seoul',
+    tel:'010'
+}
+
+console.log(user)
+console.log(user.name)
+console.log(user.address)
